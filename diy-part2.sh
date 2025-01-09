cdc0591139f1eee3faec81efa5d9545e77e0ac92
jmqyl <82797863+jmqyl@users.noreply.github.com>
2025-01-09 10:19:50 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -39,4 +39,4 @@ sed -i "s/luci-static\/bootstrap/luci-static\/argon/g" feeds/luci/modules/luci-b
 ##### K2P-32M修改编译文件 ######
 sed -i '/spi-max-frequency/a\\t\tbroken-flash-reset;' target/linux/ramips/dts/mt7621_phicomm_k2p.dts
 sed -i 's/<0xa0000 0xf60000>/<0xa0000 0x1fb0000>/g' target/linux/ramips/dts/mt7621_phicomm_k2p.dts
-sed -i 's/16064k/32448k/g' target/linux/ramips/image/mt7621.mk
+sed -i 's/15744k/32448k/g' target/linux/ramips/image/mt7621.mk
